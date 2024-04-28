b0616219a0a1d3f938e04746cdf159d70111d38b
RenterRus <artur.dawydov2013@yandex.ru>
2024-04-28 22:01:31 +0300

    stash

--- internal/filer/mdsher.go
+++ internal/filer/mdsher.go
@@ -16,12 +16,19 @@ func NewFiler() Filer {
 type mdsher struct {
 }
 
+/*
+TODO: Добавить префикс для расположений файлов (через конфиг)
+TODO: Научить создавать директории при необходимости
+TODO: Добавить в файлы уникальные идентификаторы (например, по номеру топика).
+		Для пердотвращения коллизий в случае двух оджинаковых имен файлов в разных топиках
+*/
+
 func (m *mdsher) Upsert(ctx context.Context, files []Files, userID string) ([]FilesResponse, error) {
 	if files != nil {
 		resp := make([]FilesResponse, len(files))
 
 		for i := range files {
-			file, err := os.Create(fmt.Sprintf("/%s/notis/%s.md", userID, files[i]))
+			file, err := os.Create(fmt.Sprintf("./%s/notis/%s.md", userID, files[i]))
 			if err != nil {
 				log.Warn().Msg(fmt.Sprintf("file cannot create: %v", err))
 			}
@@ -45,7 +52,7 @@ func (m *mdsher) Upsert(ctx context.Context, files []Files, userID string) ([]Fi
 	return nil, ErrFilesEmpty
 }
 func (m *mdsher) Read(ctx context.Context, filename string, userID string) (string, error) {
-	file, err := os.Open(fmt.Sprintf("/%s/notis/%s.md", userID, filename))
+	file, err := os.Open(fmt.Sprintf("./%s/notis/%s.md", userID, filename))
 	if err != nil {
 		return "", ErrFilesCannotRead
 	}
